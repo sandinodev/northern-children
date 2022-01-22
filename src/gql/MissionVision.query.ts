ad9bc42ee9da4868748be666278097f013cb931b
aleksandra <kjerte@gmail.com>
2022-01-22 22:32:58 +0100

    fix MissionVision filename

--- src/gql/MissionVision.query.ts
+++ src/gql/MissionVision.query.ts
@@ -3,6 +3,7 @@ import gql from "graphql-tag";
 import { PILL_FRAGMENT } from "~/components/pills";
 import { QUOTE_FRAGMENT } from "~/components/quote";
 import { SEO_FRAGMENT } from "~/components/seo";
+import { SLIDER_VERTICAL_SLIDE_FRAGMENT } from "~/components/slider";
 import { TEXT_MESSAGE_FRAGMENT } from "~/components/text";
 
 import { ASSET_FRAGMENT } from "./Fragments.fragment";
@@ -19,6 +20,7 @@ export const MISSION_VISION_QUERY = gql`
             text
           }
         }
+        historyTitle
         message {
           ...TextMessage
         }
@@ -31,6 +33,9 @@ export const MISSION_VISION_QUERY = gql`
         seo {
           ...Seo
         }
+        sliderVertical {
+          ...SliderVerticalSlide
+        }
         text: textWithLineBreaks
         teamEffortTitle: text
         teamEffortText: textAdditional
@@ -42,5 +47,6 @@ export const MISSION_VISION_QUERY = gql`
   ${PILL_FRAGMENT}
   ${QUOTE_FRAGMENT}
   ${SEO_FRAGMENT}
+  ${SLIDER_VERTICAL_SLIDE_FRAGMENT}
   ${TEXT_MESSAGE_FRAGMENT}
 `;
--- src/gql/index.ts
+++ src/gql/index.ts
@@ -1,3 +1,4 @@
 export { GLOBALS_QUERY } from "./Globals.query";
 export { HOME_QUERY } from "./Home.query";
-export { MISSION_VISION_QUERY } from "./MissionVision.fragment";
+export { MISSION_VISION_QUERY } from "./MissionVision.query";
+export { TEAM_QUERY } from "./Team.query";
