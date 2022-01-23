ed31fb0f6131893f0e6a917df0ba8bba50a3b711
aleksandra <kjerte@gmail.com>
2022-01-23 11:25:59 +0100

    add MAIN_PADDING_TOP, PAGES_WITHOUT_FOOTER and PAGES_WITHOUT_FOOTER_CTA constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,12 @@
+export const MAIN_PADDING_TOP = {
+  min: 60,
+  max: 200,
+};
+
+export const PAGES_WITHOUT_FOOTER = ["/team/[slug]"];
+
+export const PAGES_WITHOUT_FOOTER_CTA = ["/contact"];
+
 export const WRAPPER_PADDING = {
   min: 10,
   max: 60,
