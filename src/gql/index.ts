d6e30818383fa580510d4124eb59c48f5d45f92a
aleksandra <kjerte@gmail.com>
2022-01-23 22:09:35 +0100

    add exports

--- src/gql/index.ts
+++ src/gql/index.ts
@@ -1,6 +1,9 @@
 export { GLOBALS_QUERY } from "./Globals.query";
 export { HOME_QUERY } from "./Home.query";
+export { JOIN_QUERY } from "./Join.query";
 export { MISSION_VISION_QUERY } from "./MissionVision.query";
 export { PEOPLE_SLUGS_QUERY } from "./People.query";
 export { PERSON_QUERY } from "./Person.query";
+export { SERVICE_QUERY } from "./Service.query";
+export { SERVICES_SLUGS_QUERY } from "./Services.query";
 export { TEAM_QUERY } from "./Team.query";
