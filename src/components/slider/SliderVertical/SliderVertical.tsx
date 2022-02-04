dc70a801889170647b633acbc8645f5e1ab1b222
aleksandra <kjerte@gmail.com>
2022-02-04 12:50:11 +0100

    finish SliderVertical component

--- src/components/slider/SliderVertical/SliderVertical.tsx
+++ src/components/slider/SliderVertical/SliderVertical.tsx
@@ -1,11 +1,114 @@
-import { BaseWrapper } from "~/components/base";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect, useRef } from "react";
+import tw, { css, styled } from "twin.macro";
+
+import { BaseContainer, BaseImage, BaseText, BaseWrapper } from "~/components/base";
+
+import { useBreakpoints } from "~/hooks";
 
 import { SliderVerticalSlideFragment } from "~/types";
+
+import { isClient } from "~/utils";
+
+if (isClient) {
+  gsap.registerPlugin(ScrollTrigger);
+}
 
 interface Props {
   slides: SliderVerticalSlideFragment[];
 }
 
+const BACKGROUNDS = [tw`bg-turquoise`, tw`bg-yellow`, tw`bg-blue`, tw`bg-red`, tw`bg-violet`];
+
+const Wrapper = styled.section``;
+
+const Slide = styled(BaseWrapper)<{ i: number }>`
+  ${tw`relative lg:h-screen pb-100 lg:pb-0`}
+
+  ${({ i }) => css`
+    ${BACKGROUNDS[gsap.utils.wrap(0, BACKGROUNDS.length, i)]}
+  `}}
+  `;
+
+const Container = tw(BaseContainer)`items-start`;
+
+const Title = styled.h3`
+  ${tw`col-span-full lg:mt-200 mb-40 text-lg`}
+`;
+
+const Text = tw.div`col-span-full lg:col-span-5 mb-40 lg:mb-0 lg:text-md`;
+
+const Image = styled.div`
+  ${tw`relative col-span-full lg:absolute lg:top-0 lg:right-0 lg:w-1/2 lg:h-full`}
+`;
+
 export const SliderVertical = ({ slides }: Props) => {
-  return <BaseWrapper>slider</BaseWrapper>;
+  const refs = {
+    root: useRef<HTMLDivElement>(null),
+    sts: useRef<gsap.plugins.ScrollTriggerInstance[]>([]),
+  };
+
+  const { lg } = useBreakpoints();
+
+  useEffect(() => {
+    if (!lg) {
+      return refs.sts.current.forEach((st) => st.kill());
+    }
+
+    if (!refs.root.current || refs.sts.current.length) return;
+
+    const slides = refs.root.current.querySelectorAll(".slider-vertical__slide");
+    const slidesAmount = slides.length - 1;
+
+    [...slides].forEach((slide, i) => {
+      const isLast = i === slidesAmount;
+      const slideContainer = slide.querySelector(".slider-vertical__slide-container");
+      const tween = isLast
+        ? undefined
+        : gsap.to(slideContainer, { autoAlpha: 0, duration: 1, ease: "none", paused: true });
+
+      refs.sts.current.push(
+        ScrollTrigger.create({
+          trigger: slide,
+          start: "top top",
+          end: isLast ? "top bottom" : "bottom top",
+          endTrigger: isLast ? "footer" : slide,
+          anticipatePin: 1,
+          pin: true,
+          pinSpacing: false,
+          onUpdate: ({ progress }) => {
+            if (progress >= 0.5 && tween) {
+              tween.progress(gsap.utils.normalize(0.5, 1, progress));
+            }
+          },
+        })
+      );
+    });
+
+    return () => {
+      refs.sts.current.forEach((st) => st.kill());
+    };
+  }, [lg, refs.root, refs.sts]);
+
+  if (!slides.length) return null;
+
+  return (
+    <Wrapper ref={refs.root}>
+      {slides.map(({ image, text, title }, i) => (
+        <Slide key={i} className="slider-vertical__slide" i={i}>
+          <Container className="slider-vertical__slide-container">
+            <Title>{title}</Title>
+            <Text>
+              <BaseText text={text} />
+            </Text>
+
+            <Image>
+              <BaseImage absolute={lg} fullH={lg} fullW {...image?.[0]} />
+            </Image>
+          </Container>
+        </Slide>
+      ))}
+    </Wrapper>
+  );
 };
--- src/components/slider/SliderVertical/SliderVerticalSlide.fragment.ts
+++ src/components/slider/SliderVertical/SliderVerticalSlide.fragment.ts
@@ -1,8 +1,15 @@
 import gql from "graphql-tag";
 
+import { ASSET_FRAGMENT } from "~/gql/Fragments.fragment";
+
 export const SLIDER_VERTICAL_SLIDE_FRAGMENT = gql`
   fragment SliderVerticalSlide on sliderVertical_slide_BlockType {
+    image {
+      ...Asset
+    }
     text
     title: slideTitle
   }
+
+  ${ASSET_FRAGMENT}
 `;
