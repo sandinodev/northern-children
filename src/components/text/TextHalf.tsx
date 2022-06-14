61522c107dc3f43cfa9f9d837a472532f983a2a3
Sandino <sandy@thefutureforward.com>
2022-06-14 22:53:02 +0800

    tweaks

--- src/components/text/TextHalf.tsx
+++ src/components/text/TextHalf.tsx
@@ -15,10 +15,10 @@ export const TextHalf = ({ text }: Props) => {
 
   return (
     <Wrapper>
-      <div className="BaseSection__Title-sc-2mfa5o-0 fXORvl">
-        <div className="MaskOpacity__Wrapper-pe5v27-0 fDCtjV mask mask--is-visible">Our Vision</div>
-      </div>
       <BaseContainer>
+        <div className="BaseSection__Title-sc-2mfa5o-0 fXORvl">
+          <div className="MaskOpacity__Wrapper-pe5v27-0 fDCtjV mask mask--is-visible">Our Vision</div>
+        </div>
         <StyledBaseText text={text} />
       </BaseContainer>
     </Wrapper>
