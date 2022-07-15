4ca18f63f3c5e8c3baac473ecc42a3b97d0095f9
Sandino <sandy@thefutureforward.com>
2022-07-15 18:11:16 +0800

    ga added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,23 @@ class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+            `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
